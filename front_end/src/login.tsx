5cac31025eead96d156392e2b90b8ce536d1e8b6
joaovictorximenes0-eng <joaovictorximenes0@gmail.com>
2026-06-27 18:44:57 -0300

    commit

--- front_end/src/login.tsx
+++ front_end/src/login.tsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Input from "./components/Input.tsx";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center bg-[#161410]">
+    <form
+      className="flex h-screen items-center justify-center bg-[#161410]"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-col items-center justify-center gap-2 rounded-md">
         <img src="./logo.png" alt="" className="mb-4" />
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="w-full cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white">
           Login
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
