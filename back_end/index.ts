859f00c7ccaab9db5366ca30f0212b962a1c8d8c
joaovictorximenes0-eng <joaovictorximenes0@gmail.com>
2026-06-28 18:15:40 -0300

    Class 29

--- back_end/index.ts
+++ back_end/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Hello World");
+app.get("/teste", (req,res) => {
+    res.json("Você acessou a rota inicial");
+    console.log("Rota inicial acessada");
+});
+
+app.listen(3000, () => {
+  console.log("Hello World");
 });
